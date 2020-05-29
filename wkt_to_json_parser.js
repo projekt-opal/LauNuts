15c64849b3b8111492002f49c5be0228ff18f16c
Gourab Sahu <gourab@mail.uni-paderborn.de>
2020-05-29 15:54:59 +0200

    Use double quote for strings

--- wkt_to_json_parser.js
+++ wkt_to_json_parser.js
@@ -1,8 +1,8 @@
-var fs = require('fs');
+var fs = require("fs");
 
-var parse = require('wellknown');
+var parse = require("wellknown");
 
-var file_path_to_write='src\\main\\resources\\launuts_geojson_and_shape_files\\node_response.json';
+var file_path_to_write="src\\main\\resources\\launuts_geojson_and_shape_files\\node_response.json";
 
 fs.writeFile(file_path_to_write, JSON.stringify(parse(process.argv.slice(2)[0])), function (err,data) {
 	if (err) {
