e535dcceec28a4349f6fc3fc8727b7afc93509c5
Gourab Sahu <gourab@mail.uni-paderborn.de>
2020-05-30 04:07:21 +0200

    Add comments for hole and hole-holder

--- src/test/java/org/dice_research/opal/launuts/polygons/parser/NutParserTest.java
+++ src/test/java/org/dice_research/opal/launuts/polygons/parser/NutParserTest.java
@@ -14,16 +14,19 @@ public class NutParserTest {
 	NutsParser nut_parser = new NutsParser();
 	//nut_parser.createNutPolygons();
 	
-	//Enzkreis has a hole Pforzheim_Stadtkreis
+	//Pforzheim_Stadtkreis is a hole inside pforzheim_stadtkreis
 	private static String nuts_code_of_Enzkreis = "DE12B";
 	private static String nuts_code_of_Pforzheim_Stadtkreis = "DE129";
 	
+	//Eisenach_Kreisfreie_Stadt is a hole inside Wartburgkreis
 	private static String nuts_code_of_Wartburgkreis = "DEG0P";
 	private static String nuts_code_of_Eisenach_Kreisfreie_Stadt = "DEG0N";
 	
+	//Weimar Kreisfreie_Stadt is a hole inside Weimarer Land
 	private static String nuts_code_of_Weimarer_Land = "DEG0G";
 	private static String nuts_code_of_Weimar_Kreisfreie_Stadt = "DEG05";
 	
+	//Landau_in_der_Pfalz_Kreisfreie_Stadt is a hole inside Südliche_Weinstraße
 	private static String nuts_code_of_Südliche_Weinstraße = "DEB3H";
 	private static String nuts_code_of_Landau_in_der_Pfalz_Kreisfreie_Stadt = "DEB33";
 
