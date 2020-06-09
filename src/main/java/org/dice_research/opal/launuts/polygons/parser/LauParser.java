da50ed917ad71bbfc2fd8e283b635cbcab59b74d
Amit Kumar <amit51rajput3@gmail.com>
2020-06-09 06:46:48 +0200

    Implements optimization to Centroid computation

--- src/main/java/org/dice_research/opal/launuts/polygons/parser/LauParser.java
+++ src/main/java/org/dice_research/opal/launuts/polygons/parser/LauParser.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-import javax.validation.OverridesAttribute;
+
 
 import org.json.simple.parser.JSONParser;
 import org.apache.commons.io.IOUtils;
--- src/test/java/org/dice_research/opal/launuts/polygons/parser/LauNutsPolygonCentroidTest.java
+++ src/test/java/org/dice_research/opal/launuts/polygons/parser/LauNutsPolygonCentroidTest.java
@@ -2,6 +2,7 @@ package org.dice_research.opal.launuts.polygons.parser;
 
 import org.dice_research.opal.launuts.polygons.*;
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 /*
@@ -13,20 +14,41 @@ import static org.junit.Assert.assertEquals;
 
 
 public class LauNutsPolygonCentroidTest {
-	LauNutsPolygonCentroid LauNutsObj = new LauNutsPolygonCentroid();
-	@Test
-	public void testCentroidDE713() throws Exception{
-		// NUTS code DE: DEF01
-		// LAU code DE: 01001000
-		//Test 2: Test to check if the LAU and NUTS code are available in list
-		assertEquals("8.79162,50.108166", LauNutsObj.getNutsCenterPoints("DE713").toString());
-	}
+    LauNutsPolygonCentroid LauNutsObj = new LauNutsPolygonCentroid();
 
-	@Test
-	public void testCentroidDE122() throws Exception{
-		// NUTS code DE: DEF01
-		// LAU code DE: 01001000
-		//Test 2: Test to check if the LAU and NUTS code are available in list
-		assertEquals("8.39315,49.029606", LauNutsObj.getNutsCenterPoints("DE122").toString());
-	}
+    @Test
+    public void testcentroidcomputationDE713() throws Exception{
+        // NUTS code DE: DEF01
+        //Test 2: Test to check if the correct centroid computation is done
+        assertEquals("8.79162,50.108166", LauNutsObj.iteratingNuts("DE713").toString());
+    }
+
+    @Test
+    public void testcentroidcomputationDE122() throws Exception {
+        // NUTS code DE: DEF01
+        //Test 2: Test to check if the correct centroid computation is done
+        assertEquals("8.39315,49.029606", LauNutsObj.iteratingNuts("DE122").toString());
+    }
+
+    @Test
+    public void testcentroidcomputationDEG() throws Exception {
+        // NUTS code DE: DEF01
+        //Test 2: Test to check if the correct centroid computation is done
+        assertEquals("11.272815,50.90602", LauNutsObj.iteratingNuts("DEG").toString());
+    }
+
+    @Test
+    public void testcentroidcomputationDE() throws Exception {
+        // NUTS code DE: DE
+        //Test 2: Test to check if the correct centroid computation is done
+        assertEquals("11.272815,50.90602", LauNutsObj.iteratingNuts("DE").toString());
+    }
+
+    @Test
+    public void testcentroidcomputationDEF() throws Exception {
+        // NUTS code DE: DEF
+        //Test 2: Test to check if the correct centroid computation is done
+        assertEquals("11.272815,50.90602", LauNutsObj.iteratingNuts("DE").toString());
+    }
+
 }
