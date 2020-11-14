d9ddf282770c650014c5b0c3df092d035d2a9bbf
Adrian Wilke <adibaba@users.noreply.github.com>
2020-11-14 21:55:40 +0100

    0.4.1-SNAPSHOT

--- src/main/java/org/dice_research/opal/launuts/Main.java
+++ src/main/java/org/dice_research/opal/launuts/Main.java
@@ -29,7 +29,7 @@ public class Main {
 	/**
 	 * Version from pom.xml
 	 */
-	public static final String VERSION = "0.4.0";
+	public static final String VERSION = "0.4.1-SNAPSHOT";
 
 	private static final Logger LOGGER = LogManager.getLogger();
 	private Cfg cfg;
